289982e9b2f2626744b34a933a767723144581b8
Alexander Tokarev <alex.tokarew@gmail.com>
2022-03-08 16:45:49 +0300

    Pushing notifications

--- app/src/main/java/com/github/alextokarew/buckshunter/ApiPollWorker.kt
+++ app/src/main/java/com/github/alextokarew/buckshunter/ApiPollWorker.kt
@@ -1,7 +1,13 @@
 package com.github.alextokarew.buckshunter
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
+import android.content.SharedPreferences
 import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.preference.PreferenceManager
 import androidx.work.*
 import java.util.concurrent.TimeUnit
@@ -10,6 +16,7 @@ class ApiPollWorker(val context: Context, workerParams: WorkerParameters) : Work
 
     companion object {
         val TAG = "api.poll.worker"
+        val CHANNEL_ID = "new.atm.channel"
 
         fun scheduleNextExecution(context: Context) {
             Log.i("Poll worker", "Trying to enqueue the next execution")
@@ -26,10 +33,39 @@ class ApiPollWorker(val context: Context, workerParams: WorkerParameters) : Work
 
     override fun doWork(): Result {
         val prefs = PreferenceManager.getDefaultSharedPreferences(context)
-        Log.i("Poll worker", "Reading distance from prefs: ${prefs.getInt(context.resources.getString(R.string.pref_max_distance), -1)}")
+        doIteration(prefs)
         if (prefs.getBoolean(context.resources.getString(R.string.pref_scan_enabled), false)) {
             scheduleNextExecution(context)
         }
         return Result.success()
+    }
+
+    private fun doIteration(prefs: SharedPreferences) {
+        Log.i("Poll worker", "Reading distance from prefs: ${prefs.getInt(context.resources.getString(R.string.pref_max_distance), -1)}")
+        //Query coordinartes
+        //Push notification
+        createNotificationChannel()
+        val nm = NotificationManagerCompat.from(context)
+        val notification = NotificationCompat.Builder(context, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle("New ATM found!")
+            .setContentText("ID: id, Address: address")
+            .build()
+        val notificationId: Int = (System.currentTimeMillis() / 1000).toInt()
+        nm.notify(notificationId, notification)
+    }
+
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+
+        val importance = NotificationManager.IMPORTANCE_HIGH
+        val channel = NotificationChannel(CHANNEL_ID, "ATM Alerts", importance).apply {
+            description = "ATM alerting channel"
+            enableVibration(true)
+        }
+        // Register the channel with the system
+        val notificationManager: NotificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
     }
 }
